25394dec1e2571246f8fb03100388503d6e02923
Faruq Hossen <faruqhossen30@gmail.com>
2022-04-16 22:03:17 +0600

    react added in homepage

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Example() {
+function App() {
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header">App Component</div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">I'm an App text component!</div>
                     </div>
                 </div>
             </div>
@@ -17,8 +17,8 @@ function Example() {
     );
 }
 
-export default Example;
+export default App;
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+if (document.getElementById('root')) {
+    ReactDOM.render(<App />, document.getElementById('root'));
 }
