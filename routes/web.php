2ab6c5344fb74270bd76d2c1fc4f6b6e5576f9c0
Md. Aminul Islam <aminulislammd97@gmail.com>
2022-05-26 18:28:28 +0600

    FrontEnd change Frontend

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,15 @@ use App\Http\Controllers\Backend\DashboardController;
 use App\Http\Controllers\Backend\Location\DivisionController;
 use App\Http\Controllers\Backend\Vot\VotController;
 use App\Http\Controllers\Frontend\CategoryNewsController;
-use App\Http\Controllers\FrontEnd\CategorywisenewsController;
-use App\Http\Controllers\FrontEnd\HomepageController;
+use App\Http\Controllers\Frontend\CategorywisenewsController;
+use App\Http\Controllers\Frontend\HomepageController;
 use App\Http\Controllers\Frontend\LocationwisenewsController;
 use App\Http\Controllers\Frontend\LpostController;
 use App\Http\Controllers\Frontend\SearchController;
 use Illuminate\Support\Facades\Auth;
 
 
-use App\Http\Controllers\FrontEnd\SinglepostController;
+use App\Http\Controllers\Frontend\SinglepostController;
 use App\Http\Controllers\Frontend\SubcategorypostController;
 use App\Http\Controllers\Frontend\SubCategorywiseController;
 
